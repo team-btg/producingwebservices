86dffb5b2af334fd31ac1da3acfa8a845d4954bc
team-btg <bryan_gomez032000@yahoo.com>
2024-02-28 19:52:21 +0000

    Initial commit

---
+++ src/main/java/com/example/producingwebservice/CountryEndpoint.java
@@ -0,0 +1,36 @@
+package com.example.producingwebservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import io.spring.guides.gs_producing_web_service.GetCountryRequest;
+import io.spring.guides.gs_producing_web_service.GetCountryResponse;
+
+import javax.xml.namespace.QName;
+
+@Endpoint
+public class CountryEndpoint {
+    private static final String NAMESPACE_URI = "http://spring.io/guides/gs-producing-web-service";
+
+    private CountryRepository countryRepository;
+
+    @Autowired
+    public CountryEndpoint(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+    @ResponsePayload
+    public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+        GetCountryResponse response = new GetCountryResponse();
+        System.out.println(request.getName());
+
+        response.setCountry(countryRepository.findCountry(request.getName()));
+
+        return response;
+    }
+}
+
---
+++ src/main/java/com/example/producingwebservice/ProducingwebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.producingwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducingwebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducingwebservicesApplication.class, args);
+	}
+
+}
